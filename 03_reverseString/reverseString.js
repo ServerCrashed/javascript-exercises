a37d81c6f0b2ab86387c9608397bb274477c3b25
ServerCrashed <aks3045@proton.me>
2025-02-15 14:35:20 +0530

    Pass Test 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    string = "";
+    a = text.split("");
+    for(let i=0; i<text.length; i++) string+=a.pop();
+    return string;
 };
 
 // Do not edit below this line
