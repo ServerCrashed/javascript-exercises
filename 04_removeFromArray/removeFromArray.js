ca6084b1988386448487cd44bc7066045770acc9
ServerCrashed <aks3045@proton.me>
2025-02-15 15:06:30 +0530

    Pass Test 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...o) {
+    for(let i=0; i<array.length; i++){
+        if(o.includes(array[i])) {
+            array.splice(i,1);
+            i--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
