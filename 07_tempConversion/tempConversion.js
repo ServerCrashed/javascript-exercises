1b9f4eaeb41676241f1dc425e3dc947dc117cef8
ServerCrashed <aks3045@proton.me>
2025-02-15 18:10:14 +0530

    Pass Test 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const tempResult = (f-32)*(5/9);
+  if(tempResult==0) return 0;
+  else return +tempResult.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  const tempResult = c*(9/5)+32;
+  if(tempResult==0) return 0;
+  else return +tempResult.toFixed(1);
 };
 
 // Do not edit below this line
