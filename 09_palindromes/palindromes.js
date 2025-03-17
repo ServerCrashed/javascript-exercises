395fec8d46b9efea57dc3f9b4806d7dfa219bdeb
ServerCrashed <aks3045@proton.me>
2025-03-18 00:29:35 +0530

    Pass Test 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let mainString = mainstr(s);
+    reverseString = mainString.split('').reverse().join('');
+    return mainString === reverseString;
 };
+
+function isChar(char){
+    return (/[a-zA-Z]/).test(char);
+}
+
+function mainstr(st){
+    let an = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    st = st.toLowerCase();
+    let m = '';
+    for(i of st){
+        if(an.includes(i)) m+=i;
+    }
+    return m;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
